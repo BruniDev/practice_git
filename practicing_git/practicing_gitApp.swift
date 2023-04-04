531504f8ea409d750b0f554c83e1908e7fc68e64
Junghyeon Kim <hyeondxx@gmail.com>
2023-04-04 23:13:22 +0900

    first commit

---
+++ practicing_git/practicing_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicing_gitApp.swift
+//  practicing_git
+//
+//  Created by 정현 on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct practicing_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
